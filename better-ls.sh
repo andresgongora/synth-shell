d205536a7efeab6b47c77ba255b3e549f0c84a58
Andres Gongora <andresgongora@uma.es>
2018-03-02 12:52:30 +0100

    Overall improvements to robustness

--- better-ls.sh
+++ better-ls.sh
@@ -46,37 +46,65 @@
 ##  FUNCTION                                                                  ##
 ################################################################################
 
-function better-ls()
-{
-	if [ $# -eq 0 ]		# If no arguments passed
-	then
+##	NOTES
+##	=====
+##
+## 	shopt -s extglob 
+##		This must be enabled for extglob wildcards to work (eg !)
+##
+## 	files=$(/usr/bin/ls * 2> /dev/null | wc -l)
+##		Create a list of all files in '*' and count it with wc -l
+##		If zero there is no file
+##
+##	hidden_files=$(/usr/bin/ls -d .!(|.) 2> /dev/null | wc -l)
+##		Same as above, but for '.!(|.)', which includes all
+##		hidden files but ommits '.' and '..'
+##
+##	.!(|.)
+##		Anything starting with '.' and not followed by '|.',
+##		meaning either nothing or another '.'
+##
 
-		## Implied . and .., visible folders, then visible files
-		/usr/bin/ls -d {.,..,*} -lA --color=auto --human-readable --time-style=long-iso --group-directories-first;
-	
-		
-		## Then hidden directories and finally hidden files
-		## -d		list folders/files themselves, not their content
-		## .!(|.)	anything starting with '.' and not followed by '|.', meaning either nothing or another '.'
-		if [ -d .!(|.) ]
+
+shopt -s extglob
+
+
+## IF NO ARGUMENTS PASSED -> run better ls version on current folder
+if [ $# -eq 0 ]		# If no arguments passed
+then
+
+	## IF THE CURRENT FOLDER IS NOT EMPTY -> Display all
+	files=$(/usr/bin/ls * 2> /dev/null | wc -l)	
+	if [ "$files" != "0" ]
+	then 
+		## List implied . and .., visible folders, then visible files
+		/usr/bin/ls -d {.,..,*} -lA --color=auto --human-readable \
+			--time-style=long-iso --group-directories-first;
+
+
+		## List hidden folders and files (only if they exist)
+		hidden_files=$(/usr/bin/ls -d .!(|.) 2> /dev/null | wc -l)	
+		if [ "$hidden_files" != "0" ]
 		then
 			echo ""
-			/usr/bin/ls -d .!(|.) -l --color=auto --hide='..' --human-readable --time-style=long-iso --group-directories-first;
+			/usr/bin/ls -d .!(|.) -l --color=auto --hide='..' \
+				--human-readable --time-style=long-iso \
+				--group-directories-first;
 		fi
-		
+
+	## IF THE CURRENT FOLDER IS EMPTY -> List . and ..
 	else
-		## Add user argument
-		/usr/bin/ls -la --color=auto --human-readable --time-style=long-iso --group-directories-first "$@";	
+		/usr/bin/ls -d {.,..,} -lA --color=auto --human-readable \
+			--time-style=long-iso --group-directories-first;
 	fi
-}
 
 
+## IF ARGUMENTS PASSED -> run standard ls but with some tweaks (eg: colors)		
+else
+	/usr/bin/ls -la --color=auto --human-readable --time-style=long-iso \
+		--group-directories-first "$@";	
+fi
 
-################################################################################
-##  ALIAS                                                                     ##
-################################################################################
-
-alias ls='better-ls'
 
 
 ### EOF ###
