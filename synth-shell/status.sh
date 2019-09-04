17aaf687bd85751dd6c22d92114cc8e773d7a1c1
Andres Gongora <andresgongora@uma.es>
2019-09-04 16:09:52 +0200

    Fix top delay by using bc to compute a 1ms delay value
    Signed-off-by: Andres Gongora <andresgongora@uma.es>

--- synth-shell/status.sh
+++ synth-shell/status.sh
@@ -1036,11 +1036,12 @@ printHogsCPU()
 		## CALL TOP IN BATCH MODE
 		## Check if "%Cpus(s)" is shown, otherwise, call "top -1"
 		## Escape all '%' characters
-		local top=$(nice 'top' -b -d 0.1 -n 1 )
+		local delay=$(echo "scale=5; 1/1000" | bc)
+		local top=$(nice 'top' -b -d $delay -n 1 )
 		local cpus=$(echo "$top" | grep "Cpu(s)" )
 		if [ -z "$cpus" ]; then
 			echo ":("
-			local top=$(nice 'top' -b -d 0.1 -1 -n 1 )
+			local top=$(nice 'top' -b -d $delay -1 -n 1 )
 			local cpus=$(echo "$top" | grep "Cpu(s)" )
 		fi
 		local top=$(echo "$top" | sed 's/\%/\%\%/g' )
